c21bf2b98d2546f93951d1c9e6764324a8db58bf
Oleg Babin <babin.architect@gmail.com>
2023-03-25 13:39:45 +0200

    - valid Email

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -17,6 +17,6 @@ class LoginForm(FlaskForm):
 
 class RegisterForm(FlaskForm):
     username = StringField("Username", validators=[validators.DataRequired()])
-    email = EmailField("Email", validators=[validators.DataRequired(), validators.Email()])
+    email = EmailField("Email", validators=[validators.DataRequired()])
     password = PasswordField("Password", validators=[validators.DataRequired(), validators.Length(min=6)])
     submit = SubmitField("Register")
