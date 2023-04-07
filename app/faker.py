7488fb7583c08b947b4e4fd0cae6da914413a93f
Oleg Babin <babin.architect@gmail.com>
2023-04-08 01:08:13 +0300

    add profile.html

--- app/faker.py
+++ app/faker.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 from faker import Faker
 
 from app import db
-from app.models import User
+from app.models import User, Profile
 
 faker_bp = Blueprint('fake', __name__)
 faker = Faker()
@@ -13,11 +13,19 @@ faker = Faker()
 @click.argument('num', type=int)
 def users(num):
     ''' Create 'num' of fake users '''
-    users = []
+    users_pass = []
     for i in range(num):
         # generate fake username and email
         username = faker.user_name()
         email = faker.email()
+        password = faker.password()
+
+        first_name = faker.first_name()
+        last_name = faker.last_name()
+        id_number = faker.unique.random_number(digits=5)
+        linkedin_url = f"https://www.linkedin.com/in/{first_name.lower()}-{last_name.lower()}-{id_number}/"
+        id_number = faker.unique.random_number(digits=5)
+        facebook_url = f"https://www.facebook.com/{first_name.lower()}.{last_name.lower()}.{id_number}/"
 
         # get user by username and email
         user = (
@@ -34,9 +42,26 @@ def users(num):
                 username=username,
                 email=email,
             )
+            user.set_password(password)
+
             db.session.add(user)
-            users.append(user)
+            db.session.commit()
+
+            users_pass.append([username, password])
+
+            profile = Profile(
+                user_id=user.id,
+                first_name=first_name,
+                last_name=last_name,
+                linkedin=linkedin_url,
+                facebook=facebook_url
+            )
+
+            db.session.add(profile)
+            db.session.commit()
 
     # save changes in db
-    db.session.commit()
+    # db.session.commit()
     print(num, 'user added.')
+    # for me Login
+    print(users_pass)
