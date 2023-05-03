5f0b42a4d447507936c99934386770ba4af0bfc2
Oleg Babin <babin.architect@gmail.com>
2023-05-03 22:31:53 +0300

    update api init

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask_restful import Api
 from .user import UsersResource, UserResource
+from .post import PostsResource, PostResource
 
 
 api_bp = Blueprint('api', __name__, url_prefix="/api")
@@ -9,3 +10,7 @@ api = Api(api_bp)
 
 api.add_resource(UsersResource, '/users', endpoint="users_list")
 api.add_resource(UserResource, '/users/<int:user_id>', endpoint="users_details")
+
+api.add_resource(PostsResource, '/posts', endpoint="posts_list")
+api.add_resource(PostResource, '/posts/<int:post_id>', endpoint="posts_details")
+
