75a18f1656cfd5d78664a0bbe4746c90016f4ff9
Oleg Babin <babin.architect@gmail.com>
2023-05-04 14:03:45 +0300

    update init

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from flask_restful import Api
 
+from .dislike import DisLikesResource, DisLikeResource
+from .like import LikesResource, LikeResource
 from .profile import ProfileResource, ProfilesResource
 from .user import UsersResource, UserResource
 from .post import PostsResource, PostResource
@@ -16,5 +18,11 @@ api.add_resource(UserResource, '/users/<int:user_id>', endpoint="users_details")
 api.add_resource(PostsResource, '/posts', endpoint="posts_list")
 api.add_resource(PostResource, '/posts/<int:post_id>', endpoint="posts_details")
 
-api.add_resource(ProfileResource, '/profile/<int:user_id>', endpoint="profile_details")
 api.add_resource(ProfilesResource, '/profiles', endpoint="profiles_list")
+api.add_resource(ProfileResource, '/profile/<int:user_id>', endpoint="profile_details")
+
+api.add_resource(LikesResource, '/likes', endpoint="likes_list")
+api.add_resource(LikeResource, '/like/<int:like_id>', endpoint="like_details")
+
+api.add_resource(DisLikesResource, '/dislikes', endpoint="dislikes_list")
+api.add_resource(DisLikeResource, '/dislike/<int:dislike_id>', endpoint="dislike_details")
